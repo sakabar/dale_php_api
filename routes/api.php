462173cbc5099d9a02f0b7bb503e6cabbec4cba6
Takafumi Sakakibara <tsakaki@lr.pi.titech.ac.jp>
2017-07-24 07:45:45 +0900

    New: BadMoveControllerにルーティング

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-
-Route::get('hello', function () {
-    return 'Hello World';
-});
+Route::resource('bad_moves', 'BadMoveController');
